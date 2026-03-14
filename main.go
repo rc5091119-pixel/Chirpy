0fb6db2fb8ce1182fc6991977d7ad4871289b981
Ravindra Choudhary <rc5091119@gmail.com>
2026-03-14 09:15:21 +0000

    Adding filtpath

--- main.go
+++ main.go
@@ -7,15 +7,18 @@ import (
 
 func main() {
 	const port = "8080"
+	const filepathroot = "."
 
 	mux := http.NewServeMux()
+	fs := http.FileServer(http.Dir(filepathroot))
+	mux.Handle("/", fs)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
 
-	log.Printf("Serving on port: %s\n", port)
+	log.Printf("Serving from %s on port: %s\n", filepathroot, port)
 	log.Fatal(srv.ListenAndServe())
 
 }
