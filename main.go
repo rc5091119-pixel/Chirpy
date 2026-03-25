bc87ca590af5bad6d980279564f0c2ebafcf4e7e
Ravindra Choudhary <rc5091119@gmail.com>
2026-03-25 12:06:23 +0000

    Adding count , reset

--- main.go
+++ main.go
@@ -1,18 +1,57 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	count := cfg.fileserverHits.Load()
+
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", count)))
+}
+
+func (cfg *apiConfig)resetHandler(w http.ResponseWriter,r *http.Request){
+	cfg.fileserverHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Counter reset"))
+}
 
 func main() {
 	const port = "8080"
 	const filepathroot = "."
 
 	mux := http.NewServeMux()
-	fs := http.FileServer(http.Dir(filepathroot))
-	mux.Handle("/", fs)
 
+	// health endpoint
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+	apiConfig := &apiConfig{}
+	
+	// static file server
+	fs := http.FileServer(http.Dir(filepathroot))
+	mux.Handle("/app/", apiConfig.middlewareMetricsInc(http.StripPrefix("/app/", fs)))
+	mux.HandleFunc("/metrics",apiConfig.metricsHandler)
+	mux.HandleFunc("/reset",apiConfig.resetHandler)
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
@@ -20,5 +59,4 @@ func main() {
 
 	log.Printf("Serving from %s on port: %s\n", filepathroot, port)
 	log.Fatal(srv.ListenAndServe())
-
 }
